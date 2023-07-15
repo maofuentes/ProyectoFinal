02d540485c245de75739734040a119e9c6c25850
maofuentes <mfuentesoliva@revolt.cl>
2023-07-14 22:22:03 -0400

    Proyecto 14 Julio 22:22

--- revolt/revolt/urls.py
+++ revolt/revolt/urls.py
@@ -15,18 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from productos import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", views.home, name="home"),
-    path("home/", views.home, name="home"),
-    path("listar_usuarios/", views.listar_usuarios, name="listar_usuarios"),
-    path("crear_usuarios/", views.crear_usuarios, name="crear_usuarios"),
-    path("login_usuarios/", views.login_usuarios, name="login_usuarios"),
-    path("datos_usuarios/", views.datos_usuarios, name="datos_usuarios"),
-    path("logout_usuarios/", views.logout_usuarios, name="logout_usuarios"),
-    path("panel_admin/", views.panel_admin, name="panel_admin"),
+    path("", include('productos.urls')),
 ]
